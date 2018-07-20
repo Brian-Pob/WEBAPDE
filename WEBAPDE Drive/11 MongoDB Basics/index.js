cb5a185f313544220d83dfc15fecc4227b4b95d8
BrianP <brian_poblete@dlsu.edu.ph>
2018-07-20 16:21:02 +0800

    Added comments to 11 and 11b and fixed bug in 11b

--- WEBAPDE Drive/11 MongoDB Basics/index.js
+++ WEBAPDE Drive/11 MongoDB Basics/index.js
@@ -8,35 +8,38 @@ const mongoClient = require('mongodb').MongoClient;
 const databaseURL = "mongodb://localhost:27017/logindb";
 
 const bodyParser = require('body-parser')
-server.use(express.json()); 
-server.use(express.urlencoded({ extended: true }));
+server.use(express.json());
+server.use(express.urlencoded({
+  extended: true
+}));
 
 server.set('view engine', 'ejs');
 
 //Only do database manipulation inside of the connection
 //When a connection is made, it will try to make the database
 //automatically. The collection(like a table) needs to be made.
-mongoClient.connect(databaseURL, function(err, db) {
+
+mongoClient.connect(databaseURL, function (err, db) {
   if (err) throw err;
   const dbo = db.db("logindb");
   //Will create a collection if it has not yet been made
-  dbo.createCollection("login", function(err, res) {
+  dbo.createCollection("login", function (err, res) {
     if (err) throw err;
-    console.log("Collection created!");
+    console.log("Collection created!"); //create database
     db.close();
   });
-}); 
-
-server.get('/', function(req, resp){
-   resp.render('./pages/index');
 });
 
-server.post('/create-user', function(req, resp){
-  mongoClient.connect(databaseURL, function(err, db) {
-    if(err) throw err;
+server.get('/', function (req, resp) {
+  resp.render('./pages/index');
+});
+
+server.post('/create-user', function (req, resp) {
+  mongoClient.connect(databaseURL, function (err, db) {
+    if (err) throw err;
     //The system will need a link to the database to be able to
     //communicate with it. Create this in the connection.
-    const dbo = db.db("logindb");
+    const dbo = db.db("logindb"); //get database
 
     //Information can be inserted into the database. The format of the
     //data will look like a JSon array. Note: Primary keys "_id" will
@@ -45,34 +48,44 @@ server.post('/create-user', function(req, resp){
       user: req.body.user,
       pass: req.body.pass
     };
-    dbo.collection("login").insertOne(insertData, function(err, res) {
-      if(err) throw err;
+
+    dbo.collection("login").insertOne(insertData, function (err, res) { //insert data into database collection
+      if (err) throw err;
       console.log("Insert Successful!");
-      db.close();
+      db.close(); //ln 38-58 is replaced by loginModel.save() in Mongoose
     });
+
   });
-  const passData = { goodStatus: 1, msg:"User created successfully" };
-  resp.render('./pages/result',{ data:passData });
+  const passData = {
+    goodStatus: 1,
+    msg: "User created successfully"
+  };
+  resp.render('./pages/result', {
+    data: passData
+  });
 });
 
-server.post('/read-user', function(req, resp){
-  mongoClient.connect(databaseURL, function(err, db) {
-    if(err) throw err;
+server.post('/read-user', function (req, resp) {
+  mongoClient.connect(databaseURL, function (err, db) {
+    if (err) throw err;
     const dbo = db.db("logindb");
     //A search query will come in the form of a JSon array as well. Make
     //sure that it follows the correct syntax.
-    const searchQuery = { user: req.body.user, pass: req.body.pass };
+    const searchQuery = {
+      user: req.body.user,
+      pass: req.body.pass //searchQuery data takes the data of the attributes
+    };
     var queryResult = 0;
 
-    dbo.collection("login").find(searchQuery).toArray(function(err, result) {
-      if(err) throw err;
+    dbo.collection("login").find(searchQuery).toArray(function (err, result) {
+      if (err) throw err;
 
       //The result, in this case, is a JSon array. This one will need to
       //go though the elements in order to find the right one. In a simple
       //login, there should be only 1 element.
       console.log(result);
 
-      if(result.length > 0 && result[0]._id != null)
+      if (result.length > 0 && result[0]._id != null) //make sure the result isn't empty
         queryResult = 1;
       db.close();
       console.log("Read Successful!");
@@ -82,33 +95,51 @@ server.post('/read-user', function(req, resp){
       //displayed will depend on the information found and so the system
       //must wait for the results first.
       var strMsg;
-      if(queryResult === 1)strMsg = "User-name and password match!";
-      else strMsg = "User-name and password do not match!";
-      const passData = { goodStatus: queryResult, msg:strMsg };
-      resp.render('./pages/result',{ data:passData });
+      if (queryResult === 1) 
+        strMsg = "User-name and password match!";
+      else 
+        strMsg = "User-name and password do not match!";
+      const passData = {
+        goodStatus: queryResult,
+        msg: strMsg
+      };
+      resp.render('./pages/result', {
+        data: passData
+      });
     });
   });
 });
 
-server.post('/update-user', function(req, resp){
-  mongoClient.connect(databaseURL, function(err, db) {
-    if(err) throw err;
+server.post('/update-user', function (req, resp) {
+  mongoClient.connect(databaseURL, function (err, db) {
+    if (err) throw err;
     const dbo = db.db("logindb");
 
     //To update a query, it will need to have a search parameter and a
     //change of values.
-    const updateQuery = { user: req.body.user };
-    const updateValues = { $set: { pass: req.body.pass } };
+    const updateQuery = {
+      user: req.body.user   //parameter
+    };
+    const updateValues = {
+      $set: {
+        pass: req.body.pass   //value to be changed
+      }
+    };
 
     //This function will only update a single entry.
-    dbo.collection("login").updateOne(updateQuery, updateValues, function(err, res) {
+    dbo.collection("login").updateOne(updateQuery, updateValues, function (err, res) {
       if (err) throw err;
       console.log("Update Successful!");
       db.close();
     });
   });
-  const passData = { goodStatus: 1, msg:"User updated successfully if exists!" };
-  resp.render('./pages/result',{ data:passData });
+  const passData = {
+    goodStatus: 1,
+    msg: "User updated successfully if exists!"
+  };
+  resp.render('./pages/result', {
+    data: passData
+  });
 });
 
 const port = process.env.PORT | 9090;
--- WEBAPDE Drive/11b Mongoose/index.js
+++ WEBAPDE Drive/11b Mongoose/index.js
@@ -5,11 +5,13 @@ const server = express();
 //and feed it the correct url to run MongoDB.
 //URL is the database it connects to.
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/logindb');
+mongoose.connect('mongodb://localhost:27017/logindb'); //with Mongoose, there's no need to pass the database URL with every function call
 
 const bodyParser = require('body-parser')
-server.use(express.json()); 
-server.use(express.urlencoded({ extended: true }));
+server.use(express.json());
+server.use(express.urlencoded({
+  extended: true
+}));
 
 server.set('view engine', 'ejs');
 
@@ -18,9 +20,15 @@ server.set('view engine', 'ejs');
 //the second array is for options. By default, Mongoose adds an extra
 //field for versioning. This will be removed.
 const loginSchema = new mongoose.Schema({
-  user: { type: String },
-  pass: { type: String }
-},{ versionKey: false });
+  user: {
+    type: String
+  },
+  pass: {
+    type: String
+  }
+}, {
+  versionKey: false
+});
 
 //Note: mongoose will add an extra 's' at the end of the schema. So even if
 //it is accessed normally, it will just include the 's' when it is seen in
@@ -30,61 +38,103 @@ const loginSchema = new mongoose.Schema({
 //with while the program is running.
 const loginModel = mongoose.model('login', loginSchema);
 
-server.get('/', function(req, resp){
-   resp.render('./pages/index');
+server.get('/', function (req, resp) {
+  resp.render('./pages/index');
 });
 
-server.post('/create-user', function(req, resp){
+server.post('/create-user', function (req, resp) {
   //Creating a new instance can be made this way.
-  const loginInstance = loginModel({
-    user: req.body.user,
+  const loginInstance = loginModel({ //loginModel is the data that is being passed to the database
+    user: req.body.user, //here you are adding the data to the loginModel
     pass: req.body.pass
   });
-  
+
   //to save this into the database, call the instance's save function.
   //it will have a call-back to check if it worked.
   loginInstance.save(function (err, fluffy) {
-    if(err) return console.error(err);
-    const passData = { goodStatus: 1, msg:"User created successfully" };
-    resp.render('./pages/result',{ data:passData });
+    if (err) return console.error(err);
+    const passData = {
+      goodStatus: 1,
+      msg: "User created successfully"
+    }; //pass the data to the result page
+    resp.render('./pages/result', {
+      data: passData
+    });
   });
+
 });
 
-server.post('/read-user', function(req, resp){
-  const searchQuery = { user: req.body.user, pass: req.body.pass };
+server.post('/read-user', function (req, resp) {
+  const searchQuery = {
+    user: req.body.user,
+    pass: req.body.pass //format the search query depending on how the data is
+                        //represented in the database
+  };
   var queryResult = 0;
 
   //The model can be found via a search query and the information is found
   //in the login function. Access the information like a JSon array.
-  loginModel.findOne(searchQuery, function (err, login) {
-    if(err) return console.error(err);
-    if(login != undefined && login._id != null)
+  loginModel.findOne(searchQuery, function (err, login) { //accessing the model through
+    if (err)                                              //loginModel
+      return console.error(err); 
+    if (login != undefined && login._id != null) //make sure the result is not empty
       queryResult = 1;
 
-      //Put the change of interface in here as the system does not load
-      //things synchronously rather asynchronously. In this case, what is
-      //displayed will depend on the information found and so the system
-      //must wait for the results first.
-      var strMsg;
-      if(queryResult === 1)strMsg = "User-name and password match!";
-      else strMsg = "User-name and password do not match!";
-      const passData = { goodStatus: queryResult, msg:strMsg };
-      resp.render('./pages/result',{ data:passData });
+    //Put the change of interface in here as the system does not load
+    //things synchronously rather asynchronously. In this case, what is
+    //displayed will depend on the information found and so the system
+    //must wait for the results first.
+    var strMsg;
+    if (queryResult === 1)
+      strMsg = "User-name and password match!";
+    else
+      strMsg = "User-name and password do not match!";
+    const passData = {
+      goodStatus: queryResult,
+      msg: strMsg
+    };
+    resp.render('./pages/result', {
+      data: passData
+    });
   });
 });
 
-server.post('/update-user', function(req, resp){
-  const updateQuery = { user: req.body.user };
+server.post('/update-user', function (req, resp) {
+  const updateQuery = {
+    user: req.body.user
+  };
 
   //To update a query, first it must found. Then afterwards, its information
   //can be edited. Call the save function to update the changes.
-  loginModel.findOne(updateQuery, function (err, login) {
-    login.pass = req.body.pass;
-    login.save(function (err, result) {
-      if (err) return console.error(err);
-      const passData = { goodStatus: 1, msg:"User updated successfully if exists!" };
-      resp.render('./pages/result',{ data:passData });
-    });
+  loginModel.findOne(updateQuery, function (err, login) { //login is the data returned
+    //does not check if the user exists first
+    if(login != undefined && login._id != null){
+      login.pass = req.body.pass;
+      login.save(function (err, result) {
+        if (err) return console.error(err);
+        const passData = {
+          goodStatus: 1,
+          msg: "User updated successfully if exists!"
+        };
+
+        resp.render('./pages/result', {
+          data: passData
+        });
+
+      });
+
+    }else{
+      const passData = {
+        goodStatus: 0,
+        msg: "User does not exist"
+      };
+
+      resp.render('./pages/result', {
+        data: passData
+      });
+    }
+      
+
   });
 });
 
