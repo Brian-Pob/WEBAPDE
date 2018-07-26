bebefd7ef264a460c947639d393771e498cce6e2
Brian Poblete <brian_poblete@dlsu.edu.ph>
2018-07-27 01:38:21 +0800

    Added Mini Challenge 3

---
+++ NodeChallenge/index.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const server = express()
+server.use(express())
+
+const cookieparser = require('cookie-parser')
+server.use(cookieparser())
+
+const bodyparser = require('body-parser')
+server.use(bodyparser())
+
+server.set('view enging', 'ejs')
+
+server.get('/', function(req, resp){
+    resp.render('./index.ejs')
+})
+
+server.get('/text', function(req, resp){
+    resp.cookie('backgroundcolor', req.query.backgroundcolor)
+    resp.cookie('fontcolor', req.query.fontcolor)
+    resp.cookie('postcolor', req.query.postcolor)
+    
+    
+    
+    resp.render('./index.ejs')
+})
+
+server.get('/preferences', function(req, resp){
+    resp.render('./prefrences.ejs')
+})
+
+
+
+const port = process.env.PORT | 9090
+server.listen(port)
+
